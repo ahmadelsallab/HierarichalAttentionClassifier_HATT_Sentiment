8916d018cbf0a4a6b11de30d558826ab26459251
ahmadelsallab <ahmad.elsallab@gmail.com>
2018-12-19 15:09:31 +0200

    Update textClassifierH_LSTM_And_ATT.py

--- cod/textClassifierH_LSTM_And_ATT.py
+++ cod/textClassifierH_LSTM_And_ATT.py
@@ -187,7 +187,7 @@ class AttLayer(Layer):
 	weighted_input = x*weights
 	return tf.reduce_sum(weighted_input, axis=1)
 
-    def get_output_shape_for(self, input_shape):
+    def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[-1])
 
 sentence_input = Input(shape=(MAX_SENT_LENGTH,), dtype='int32')
